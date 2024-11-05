bdda46827e50f3b4b789209585de6c4cf12bbf44
NikolajWild <nime0002@stud.kea.dk>
2024-11-05 13:52:47 +0100

    tilføjede delete funktion

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.Model;
+import repository.Repository;
+
 public class Service {
+
+    private final Repository repository;
+
+    public Service(Repository repository) {
+        this.repository = repository;
+    }
+
+
+    public void deleteWish(int id){
+        repository.deleteWish(id);
+    }
 }
