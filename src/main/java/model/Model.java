a06a7bc938bc0cf39a7dff7179ac198e82aadd5a
NikolajWild <nime0002@stud.kea.dk>
2024-11-05 12:53:01 +0100

    Model

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -1,4 +1,48 @@
 package model;
 
 public class Model {
+    private String name;
+    private int quantity;
+    private String description;
+    private double price;
+
+    public Model(String name, int quantity, String description, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
