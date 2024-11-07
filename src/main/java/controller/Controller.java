f2e78cc76f3afe89890ff34cefcf07a562282756
AndersNystrupJ <anje0008@stud.kea.dk>
2024-11-07 15:12:11 +0100

    Alle crud funktioner i repo + controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,9 +1,11 @@
 package controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import model.Wish;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import service.Service;
+
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 @RequestMapping("/wishes")
@@ -14,6 +16,46 @@ public class Controller {
         this.service = service;
     }
 
+    // @GetMapping("/login")
+
+    // CREATE WISH
+
+    @GetMapping("/createWish")
+    public String createWish(Model model) {
+        Wish wish = new Wish();
+        model.addAttribute("wish", wish);
+        return "createWish";
+    }
+
+    // READ WISHES
+
+    @GetMapping("/wishes")
+    public String readWishes(Model model) {
+        List<Wish> wishes = service.readWishes();
+        model.addAttribute("wishes", wishes);
+        return "readWishes";
+    }
+
+    //UPDATE WISH (Måske save funktionalitet? Måske ikke nødvendigt, når det er PostMappin)
+
+    @GetMapping("/{id}/edit")
+    public String editWish(@PathVariable("id") int wishID, Model model) {
+        Wish wish = service.findWishByID(wishID);
+        model.addAttribute("wish", wish);
+        return "edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateWish(@RequestParam int wishID,
+                             @RequestParam String name,
+                             @RequestParam int quantity,
+                             @RequestParam String description,
+                             @RequestParam double price) {
+        service.updateWish(wishID, name, quantity, description, price);
+        return "redirect:/wishes";
+    }
+
+    //Delete
 
     @PostMapping("delete/{id}")
     public String deleteWishById(@PathVariable int id) {
