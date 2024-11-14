9d80d65aa9d618facb5078ff63fedacf925e03c1
KasperHejgaard <kahe0003@stud.kea.dk>
2024-11-14 13:44:27 +0100

    azure test

---
+++ src/test/java/com/example/wishlist/WishListApplicationTest.java
@@ -0,0 +1,22 @@
+/*
+package com.example.wishlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class WishListApplicationTest {
+        @Test
+        void contextLoads() {
+            Assertions.assertTrue(true);
+        }
+
+        @Test
+        void addAttraction() {
+            Assertions.assertTrue(true);
+        }
+
+}
+*/
